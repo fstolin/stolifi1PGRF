e3349b7338c00e197e51e61333730f2442366787
Filip Stolín <stolin.f@gmail.com>
2023-05-19 13:33:55 +0200

    Initial commit

---
+++ src/project/App.java
@@ -0,0 +1,10 @@
+package project;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		new LwjglWindow(new Renderer());
+	}
+
+}
---
+++ src/project/Renderer.java
@@ -0,0 +1,79 @@
+package project;
+
+
+import org.lwjgl.glfw.*;
+
+
+/**
+* 
+* @author PGRF FIM UHK
+* @version 2.0
+* @since 2019-09-02
+*/
+public class Renderer extends AbstractRenderer{
+
+	private GLFWKeyCallback   keyCallback = new GLFWKeyCallback() {
+		@Override
+		public void invoke(long window, int key, int scancode, int action, int mods) {
+		}
+	};
+    
+    private GLFWWindowSizeCallback wsCallback = new GLFWWindowSizeCallback() {
+        @Override
+        public void invoke(long window, int w, int h) {
+        }
+    };
+    
+    private GLFWMouseButtonCallback mbCallback = new GLFWMouseButtonCallback () {
+		@Override
+		public void invoke(long window, int button, int action, int mods) {
+		}
+		
+	};
+	
+    private GLFWCursorPosCallback cpCallbacknew = new GLFWCursorPosCallback() {
+        @Override
+        public void invoke(long window, double x, double y) {
+    	}
+    };
+    
+    private GLFWScrollCallback scrollCallback = new GLFWScrollCallback() {
+        @Override public void invoke (long window, double dx, double dy) {
+        }
+    };
+ 
+/*
+	@Override
+	public GLFWKeyCallback getKeyCallback() {
+		return keyCallback;
+	}
+
+	@Override
+	public GLFWWindowSizeCallback getWsCallback() {
+		return wsCallback;
+	}
+
+	@Override
+	public GLFWMouseButtonCallback getMouseCallback() {
+		return mbCallback;
+	}
+
+	@Override
+	public GLFWCursorPosCallback getCursorCallback() {
+		return cpCallbacknew;
+	}
+
+	@Override
+	public GLFWScrollCallback getScrollCallback() {
+		return scrollCallback;
+	}
+
+	
+	@Override
+	public void init() {
+	}
+	
+	@Override
+	public void display() {
+	}*/
+}
