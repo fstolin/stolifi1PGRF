76957d0e5e7c08936c3e034385930fc4fbaed773
Filip Stolín <stolin.f@gmail.com>
2023-05-19 15:26:29 +0200

    implement GridFactory

--- src/project/GridFactory.java
+++ src/project/GridFactory.java
@@ -2,6 +2,8 @@ package project;
 
 import lwjglutils.OGLBuffers;
 
+import java.util.Arrays;
+
 public class GridFactory {
 
     /**
@@ -10,13 +12,45 @@ public class GridFactory {
      * @param n - number of column vertices
      * @return
      */
-    static OGLBuffers generateGrid(int m, int n){
+    static OGLBuffers generateGrid(int m, int n) {
         float[] vertexBuffer = new float[m * n * 2];
         // helper variable to index the vertexBuffer
         int index = 0;
 
+        // Fill the vertex Buffer
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++) {
+                vertexBuffer[index++] = (j / (float) (m - 1));
+                vertexBuffer[index++] = (i / (float) (n - 1));
+            }
+        }
 
-        return null;
+        // Index Buffer
+        int index2 = 0;
+        int[] indexBuffer = new int[6 * (m - 1) * (n - 1)];
+        for (int i = 0; i < n; i++){
+            int rowOffset = i * m;
+            for (int j = 0; j < m; j++) {
+                indexBuffer[index2++] = (j + rowOffset);
+                indexBuffer[index2++] = (j + m + rowOffset);
+                indexBuffer[index2++] = (j + 1 + rowOffset);
+
+                indexBuffer[index2++] = (j + 1 + rowOffset);
+                indexBuffer[index2++] = (j + m + rowOffset);
+                indexBuffer[index2++] = (j + m + 1 + rowOffset);
+            }
+        }
+
+        // Attributes
+        OGLBuffers.Attrib[] attributes = {
+            new OGLBuffers.Attrib("inPosition", 2)
+        };
+
+        // Return
+        return new OGLBuffers(vertexBuffer, attributes, indexBuffer);
     }
 
+    public static void main(String[] args) {
+        generateGrid(5,5);
+    }
 }
